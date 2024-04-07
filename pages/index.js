c3102d1ddeb1f1533bca08cd38f99de43205d6ea
abramsmatthew99 <50125346+abramsmatthew99@users.noreply.github.com>
2024-04-07 16:31:48 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Thugspace!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This is me, Mr. Matt, just messing around with the basic site first. 
         </p>
       </main>
 
