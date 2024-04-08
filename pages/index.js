6a563b0ca677e5769025d7abbab79129171eb4c4
abramsmatthew99 <abramsmatthew99@gmail.com>
2024-04-08 16:46:34 -0400

    Added video

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,30 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="banner">
-        Testing
+        Thugspace
       </div>
       <main>
         <Header title="Welcome to Thugspace!" />
         <p className="description">
           This is me, Mr. Matt, just messing around with the basic site first. 
         </p>
+        <div className="video-container">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/mw75ZGgdQUk?si=czjLMFHItzpzrIfT" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+          </iframe>
+        </div>
       </main>
 
       <Footer />
